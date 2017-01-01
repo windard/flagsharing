8b02efb1c3a0d5e011bd327a090ecef9306f7d51
Yang Wenqiang <1106911190@qq.com>
2017-01-01 17:17:03 +0800

    init

---
+++ chatroom.js
@@ -0,0 +1,100 @@
+var io = require('socket.io')();
+var xssEscape = require('xss-escape');
+
+var nickname_list = [];
+
+// 检查是昵称是否已经存在
+function HasNickname(_nickname){
+	for(var i=0; i<nickname_list.length; i++){
+		if(nickname_list[i] == _nickname){
+			return true;
+		}
+	}
+};
+
+// 删除昵称
+function RemoveNickname(_nickname){
+	for(var i=0; i< nickname_list.length; i++){
+		if(nickname_list[i] == _nickname){
+			nickname_list.splice(i, 1);
+		}
+	}
+}
+
+io.on('connection', function(_socket){
+	console.log(_socket.id + ':connection');
+
+	// 向当前用户发送命令和消息
+	_socket.emit('user_list', nickname_list);
+	_socket.emit('need_nickname');
+	_socket.emit('server_message','欢迎来到聊天室 :)');
+
+	// 监听当前用户的请求和数据
+
+	// 离开 
+	_socket.on('disconnect', function(){
+		console.log(_socket.id + ':disconnect');
+		if(_socket.nickname != null && _socket.nickname != ""){
+			// 广播 用户退出
+			_socket.broadcast.emit('user_quit', _socket.nickname);
+			RemoveNickname(_socket.nickname);
+		}
+	});
+
+	// 添加 和 修改 昵称
+	_socket.on('change_nickname', function(_nickname){
+		console.log(_socket.id + ': change_nickname('+_nickname+')');
+
+		_nickname = xssEscape(_nickname.trim());
+
+		// 字符长度必须小于 20 个字符
+		if(_nickname.length > 20 || _nickname.length == 0){
+			return _socket.emit('change_nickname_error', '请填写正确的用户昵称，应在4到16个字符之间。')
+		}
+
+		// 昵称重复
+		if(_socket.nickname == _nickname){
+			return _socket.emit('change_nickname_error', '你本来就叫这个名字。')
+		}
+
+		// 昵称已经被占用
+		if(HasNickname(_nickname)){
+			return _socket.emit('change_nickname_error', '此昵称已经被占用。')
+		}
+
+		var old_name = '';
+		if(_socket.nickname != '' && _socket.nickname != null){
+			old_name = _socket.nickname;
+			RemoveNickname(old_name);
+		}
+
+		nickname_list.push(_nickname);
+		_socket.nickname = _nickname;
+
+		console.log(nickname_list);
+
+		_socket.emit('change_nickname_done', _nickname);
+
+		// 广播 用户加入
+		return _socket.broadcast.emit('user_join', _nickname);
+	});
+
+	// 说话
+	_socket.on('say', function(_time, _content){
+		if('' == _socket.nickname || null == _socket.nickname){
+			return _socket.emit('need_nickname');
+		}
+
+		_content = _content.trim();
+		console.log(_socket.nickname + ': say('+_content+')');
+		// 广播 用户新消息
+		_socket.broadcast.emit('user_say', _socket.nickname, xssEscape(_content));
+		return _socket.emit('say_done', _socket.nickname, xssEscape(_content));
+	});
+
+})
+
+// 这里的listen函数在 bin/www 文件中被调用
+exports.listen = function(_server){
+	return io.listen(_server);
+}
---
+++ public/javascripts/index.js
@@ -0,0 +1,217 @@
+$(document).ready(function($) {
+	// $("#login-modal").modal({
+	// 	keyboard:false,
+	// 	backdrop:"static"
+	// 	});
+
+  var socket = io();
+
+  var chat_Utils , 		// 聊天室 工具类
+  	  chat_UI    , 		// 聊天室 界面类
+  	  chat_Socket,		// 聊天室 通信类
+
+  chat_Socket = {
+  	init:function(){
+  		console.log("Connet to Server ...");
+
+  		this.needNickname();
+  		this.setNicknameError();
+  		this.setNicknameSuccess();
+  		this.serverMessage();
+  		this.userMessage();
+  		this.userList();
+  		this.userJoin();
+  		this.userQuit();
+  		this.sayMessageSuccess();
+  	},
+  	needNickname:function(){
+  		socket.on("need_nickname", function(){
+  			$("#nickname-error").hide();
+			$("#login-modal").modal({
+				keyboard:false,
+				backdrop:"static"
+				});
+  		});
+  	},
+  	setNickname:function(_nick_name){
+  		socket.emit("change_nickname", _nick_name);
+  	},
+  	setNicknameError:function(){
+  		socket.on("change_nickname_error", function(_error_message){
+  			console.log("change_nickname_error : " + _error_message);
+  			$("#nickname-error-message").text(_error_message);
+  			$("#nickname-error").show();
+  			$('#nickname-edit').focus();
+  			// $("#login-modal").modal("show");
+  		});
+  	},
+  	setNicknameSuccess:function(){
+  		socket.on("change_nickname_done", function(_new_nickname){
+  			console.log("change_nickname_done : " + _new_nickname);
+  			$("#login-modal").modal("hide");
+  			$("#my-nickname").text(_new_nickname);
+  		});
+  	},
+  	serverMessage:function(){
+	  socket.on('server_message', function(_message){
+  		console.log("server say : " + _message);
+	  	chat_UI.serverMessage(_message, chat_Utils.getLocalHMS());
+	  });
+  	},
+  	userMessage:function(){
+  		socket.on("user_say", function(_nick_name, _message){
+  			console.log(_nick_name + " say : " + _message);
+  			chat_UI.userMessage(_nick_name, _message, chat_Utils.getLocalHMS());
+  		});
+  	},
+  	userList:function(){
+  		socket.on("user_list", function(_list){
+  			chat_UI.userList(_list);
+  		});
+  	},
+  	userJoin:function(){
+  		socket.on("user_join", function(_nick_name){
+  			console.log("user join : " + _nick_name);
+  			chat_UI.addUserToList(_nick_name);
+  			chat_UI.updateListCount();
+  			console.log("server say : "+_nick_name+" 加入了聊天室 (*^__^*) …… ");
+  			chat_UI.serverMessage(_nick_name+" 加入了聊天室 (*^__^*) …… ",chat_Utils.getLocalHMS());
+  		});
+  	},
+  	userQuit:function(){
+  		socket.on("user_quit", function(_nick_name){
+  			console.log("user quit : " + _nick_name);
+  			chat_UI.removeUserFromList(_nick_name);
+  			chat_UI.updateListCount();
+  			console.log("server say : "+_nick_name+" 离开了聊天室 ( ⊙ o ⊙ ) … ");
+  			chat_UI.serverMessage(_nick_name+" 离开了聊天室 ( ⊙ o ⊙ ) … ",chat_Utils.getLocalHMS());
+  		});
+  	},
+  	sayMessage:function(_content){
+		socket.emit("say", chat_Utils.getLocalHMS(), _content);
+  	},
+  	sayMessageSuccess:function(){
+  		socket.on("say_done", function(_nick_name, _content){
+  			console.log(_nick_name + " say : " + _content);
+  			chat_UI.userMessage(_nick_name, _content, chat_Utils.getLocalHMS());
+  		});
+  	}
+  }
+
+  chat_UI = {
+  	init:function(){
+  		this.sendMessage();
+  		this.sendNickname();
+  	},
+ 	chatBodyToBottom: function(){
+		var chat_body = $('.main .panel-body')[0];
+		chat_body.scrollTop = chat_body.scrollHeight;
+	},
+  	sendMessage:function(){
+  		var self = this;
+  		$("#send-message").on('click', function(event) {
+  			event.preventDefault();
+  			self.applyMessage();
+  		});
+		$('form').submit(function(event){
+			event.preventDefault();
+			self.applyMessage();
+		});
+  	},
+  	applyMessage:function(){
+		var content = $('input[id=input-edit]').val();
+		if(content){
+			chat_Socket.sayMessage(content);
+		}
+		$('input[id=input-edit]').val('');
+  	},
+  	sendNickname:function(){
+		var self = this;
+		$('#nickname-edit').keydown(function(_event) {
+		    if(13 == _event.keyCode) {
+		        self.applyNickname();
+		    }
+		});
+		$('#applyNicknameBtn').on('click', function(){
+			self.applyNickname();
+		})
+  	},
+  	applyNickname:function(){
+  		var nickname_edit = $("#nickname-edit");
+  		var nickname_error = $("#nickname-error");
+  		var nickname = nickname_edit.val();
+  		if ("" == nickname.trim()){
+  			$("#nickname-error-message").text("请填写昵称。");
+  			nickname_error.show();
+  			nickname_edit.focus();
+  			return;
+  		}
+  		if (nickname.length > 20 ){
+  			$("#nickname-error-message").text("昵称过长，长度应小于20个字符。");
+  			nickname_error.show();
+  			nickname_edit.focus();
+  			return;
+  		}
+  		chat_Socket.setNickname(nickname);
+  	},
+  	serverMessage:function(_content, _time){
+  		var messages = $(".msg-list-body");
+  		var message = '<div class="text-center sys-message">\
+                                <span class="sys-tip">系统消息：'+_content+'   '+_time+'</span>\
+                            </div>';
+        messages.append(message);
+        this.chatBodyToBottom();
+  	},
+  	userMessage:function(_nick_name, _content, _time){
+  		var messages = $(".msg-list-body");
+  		var nickname = $("#my-nickname").text();
+  		var textalign = _nick_name == nickname ? "text-right" : "";
+  		var label =  _nick_name == nickname ? "label-success" : "label-info";
+  		var message = '<div class="message jumbotron '+textalign+' ">\
+                                <div class="message-head">\
+                                    <span class="label '+label+'"><span class="glyphicon glyphicon-user"></span>&nbsp;&nbsp;'+_nick_name+'</span>&nbsp;&nbsp;\
+                                    <span class="label label-default"><span class="glyphicon glyphicon-time"></span>&nbsp;&nbsp;'+_time+'</span>&nbsp;&nbsp;\
+                                </div>\
+                                <div class="message-body">\
+                                   '+_content+'\
+                                </div>\
+                            </div>';
+	  	messages.append(message);
+	  	this.chatBodyToBottom();
+  	},
+  	userList:function(_user_list){
+  		$(".list .list-group").html("");
+  		for(var i=0; i < _user_list.length; i++){
+  			this.addUserToList(_user_list[i]);
+  		}
+  		this.updateListCount();
+  	},
+  	addUserToList:function(_nick_name){
+  		$(".list .list-group").append('<li class="list-group-item">'+_nick_name+'</li>')
+  	},
+  	removeUserFromList:function(_nick_name){
+  		$(".list .list-group li").each(function(){
+  			if(_nick_name == $(this).text()){
+  				$(this).remove();
+  			}
+  		});
+  	},
+  	updateListCount:function(){
+  		var list_count = $(".list .list-group").find("li").length + 1;
+  		$("#list-count").text("当前在线人数："+list_count+"人");
+  	}
+  }
+
+  chat_Utils = {
+	getLocalHMS: function(){
+		var d = new Date();
+		var t = [d.getHours(), d.getMinutes(), d.getSeconds()];
+		t = t.map(function(v, i){ return v < 10 ? '0' + v : v; })
+		return t.join(':');
+	}
+  }
+
+chat_UI.init();
+chat_Socket.init();
+
+});
