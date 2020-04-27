b9c40e62e224246b3e76d236d264a80decff33e2
yangwenqiang.ywq <yangwenqiang.ywq@bytedance.com>
2020-04-27 16:51:52 +0800

    add log
    Change-Id: I44f816d316d2398384f4f9a0d78b74c5a403f456

--- python/index.py
+++ python/index.py
@@ -4,6 +4,7 @@ import os
 import json
 import cgi
 # import bleach
+import logging
 import tornado.web
 import tornado.ioloop
 import tornado.options
@@ -12,102 +13,108 @@ import tornado.httpserver
 
 from tornado.options import define, options
 
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 define("port", default=8090, help="run on the given port", type=int)
 
 def xssProtect(message):
-	return cgi.escape(message)
-	# attributes={u'a': [u'href']}
-	# return bleach.linkify(bleach.clean(message, attributes=attributes, strip=True))
+    return cgi.escape(message)
+    # attributes={u'a': [u'href']}
+    # return bleach.linkify(bleach.clean(message, attributes=attributes, strip=True))
 
 class MainHandler(tornado.web.RequestHandler):
-	"""docstring for MainHandler"""
-	def get(self):
-		self.render("index.html", title='Flag Sharing')
+    """docstring for MainHandler"""
+    def get(self):
+        self.render("index.html", title='Flag Sharing')
 
 class ChatSocketHandler(tornado.websocket.WebSocketHandler):
-	"""docstring for ChatSocketHandler"""
-	connects = set()
+    """docstring for ChatSocketHandler"""
+    connects = set()
 
-	def open(self):
-		# print "%s connected"%(id(self))
-		self.write_message(json.dumps({"action":"needNickname", "message":""}))
+    def open(self):
+        # print "%s connected"%(id(self))
+        self.write_message(json.dumps({"action":"needNickname", "message":""}))
 
-	def on_message(self, message):
-		# XSS 过滤
-		action = xssProtect(json.loads(message)['action'])
-		message = xssProtect(json.loads(message)['message'])
-		# action = xssProtect(message[:message.index("$$")])
-		# message = xssProtect(message[message.index("$$")+2:])
-		if action == "setNickname":
+    def write_message(self, message, binary=False):
+        logger.info("message is %r", message)
+        super(ChatSocketHandler, self).write_message(message, binary)
 
-			if len(message) == 0 or len(message) > 20:
-				return self.write_message(json.dumps({"action":"setNicknameError", "message":"请填写正确的用户昵称，应在1到20个字符之间。"}))
-				# return self.write_message("setNicknameError$$请填写正确的用户昵称，应在1到20个字符之间。")
+    def on_message(self, message):
+        # XSS 过滤
+        action = xssProtect(json.loads(message)['action'])
+        message = xssProtect(json.loads(message)['message'])
+        # action = xssProtect(message[:message.index("$$")])
+        # message = xssProtect(message[message.index("$$")+2:])
+        if action == "setNickname":
 
-			if message in [x.username for x in ChatSocketHandler.connects]:
-				return self.write_message(json.dumps({"action":"setNicknameError", "message":"此昵称已经被占用。"}))
-				# return self.write_message("setNicknameError$$此昵称已经被占用。")
+            if len(message) == 0 or len(message) > 20:
+                return self.write_message(json.dumps({"action":"setNicknameError", "message":"请填写正确的用户昵称，应在1到20个字符之间。"}))
+                # return self.write_message("setNicknameError$$请填写正确的用户昵称，应在1到20个字符之间。")
 
-			self.username = message
-			self.write_message(json.dumps({"action":"setNicknameSuccess", "message":message}))
-			# self.write_message("setNicknameSuccess$$%s"%message)
-			ChatSocketHandler.send_all(json.dumps({"action":"userJoin", "message":"message"}))
-			# ChatSocketHandler.send_all("userJoin$$%s"%message)
+            if message in [x.username for x in ChatSocketHandler.connects]:
+                return self.write_message(json.dumps({"action":"setNicknameError", "message":"此昵称已经被占用。"}))
+                # return self.write_message("setNicknameError$$此昵称已经被占用。")
 
-			self.write_message(json.dumps({"action":"serverMessage", "message":"欢迎来到聊天室 :)"}))
-			# self.write_message("serverMessage$$欢迎来到聊天室 :)")
-			self.write_message(json.dumps({"action":"userList", "message":[x.username for x in ChatSocketHandler.connects]}))
-			# self.write_message("userList$$" + "$".join(x.username for x in ChatSocketHandler.connects))
-			ChatSocketHandler.connects.add(self)
+            self.username = message
+            self.write_message(json.dumps({"action":"setNicknameSuccess", "message":message}))
+            # self.write_message("setNicknameSuccess$$%s"%message)
+            ChatSocketHandler.send_all(json.dumps({"action":"userJoin", "message":"message"}))
+            # ChatSocketHandler.send_all("userJoin$$%s"%message)
 
-			# print "%s join"%self.username
-			# print [x.username for x in ChatSocketHandler.connects]
+            self.write_message(json.dumps({"action":"serverMessage", "message":"欢迎来到聊天室 :)"}))
+            # self.write_message("serverMessage$$欢迎来到聊天室 :)")
+            self.write_message(json.dumps({"action":"userList", "message":[x.username for x in ChatSocketHandler.connects]}))
+            # self.write_message("userList$$" + "$".join(x.username for x in ChatSocketHandler.connects))
+            ChatSocketHandler.connects.add(self)
 
-		elif action == "message":
-			if not self.username:
-				return self.write_message(json.dumps({"action":"needNickname", "message":""}))
-				# return self.write_message("needNickname$$")
+            # print "%s join"%self.username
+            # print [x.username for x in ChatSocketHandler.connects]
 
-			# print "%s say: %s"%(self.username, message)
+        elif action == "message":
+            if not self.username:
+                return self.write_message(json.dumps({"action":"needNickname", "message":""}))
+                # return self.write_message("needNickname$$")
 
-			try:
-				ChatSocketHandler.send_all(json.dumps({"action":"userMessage", "message":{"username":self.username, "content":message}}))
-			except Exception,e:
-				self.write_message(json.dumps({"action":"sayMessageError", "message":str(e)}))
-				# self.write_message("sayMessageError$$%s"%(str(e)))
-		else:
-			# print "%s:%s"%(action, message)
-			pass
+            # print "%s say: %s"%(self.username, message)
 
-	def on_close(self):
-		# print "%s quit"%self.username
+            try:
+                ChatSocketHandler.send_all(json.dumps({"action":"userMessage", "message":{"username":self.username, "content":message}}))
+            except Exception as e:
+                self.write_message(json.dumps({"action":"sayMessageError", "message":str(e)}))
+                # self.write_message("sayMessageError$$%s"%(str(e)))
+        else:
+            # print "%s:%s"%(action, message)
+            pass
 
-		ChatSocketHandler.connects.remove(self)
-		ChatSocketHandler.send_all(json.dumps({"action":"userQuit", "message":self.username}))
-		# print "%s closed"%(id(self))
+    def on_close(self):
+        # print "%s quit"%self.username
 
-	@classmethod
-	def send_all(cls, chat):
-		for connect in cls.connects:
-			connect.write_message(chat)
+        ChatSocketHandler.connects.remove(self)
+        ChatSocketHandler.send_all(json.dumps({"action":"userQuit", "message":self.username}))
+        # print "%s closed"%(id(self))
+
+    @classmethod
+    def send_all(cls, chat):
+        for connect in cls.connects:
+            connect.write_message(chat)
 
 class NofoundHandler(tornado.web.RequestHandler):
-	"""docstring for NofoundHandler"""
-	def get(self):
-		self.write("<h1>No Found</h1>")
-		
+    """docstring for NofoundHandler"""
+    def get(self):
+        self.write("<h1>No Found</h1>")
+        
 if __name__ == '__main__':
-	settings = {
-		"template_path": os.path.join(os.path.dirname(__file__), "template"),
-		"static_path": os.path.join(os.path.dirname(__file__), "static"),
-		'default_handler_class': NofoundHandler,
-		"debug": True,
-	}
+    settings = {
+        "template_path": os.path.join(os.path.dirname(__file__), "template"),
+        "static_path": os.path.join(os.path.dirname(__file__), "static"),
+        'default_handler_class': NofoundHandler,
+        "debug": True,
+    }
 
-	app = tornado.web.Application([
-		(r'/', MainHandler),
-		(r'/websocket', ChatSocketHandler),
-		], **settings)
-	http_server = tornado.httpserver.HTTPServer(app)
-	http_server.listen(options.port)
-	tornado.ioloop.IOLoop.instance().start()
+    app = tornado.web.Application([
+        (r'/', MainHandler),
+        (r'/websocket', ChatSocketHandler),
+        ], **settings)
+    http_server = tornado.httpserver.HTTPServer(app)
+    http_server.listen(options.port)
+    tornado.ioloop.IOLoop.instance().start()
